6b136be47cc03d8105a8ee3bea607cb1363cd8e0
David Puerta Martín <puertadavid96182@gmail.com>
2023-04-30 16:58:18 -0400

    add class-based view for posts

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -1,6 +1,17 @@
 from django.shortcuts import render, get_object_or_404
 from .models import Post
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+from django.views.generic import ListView
+
+
+class PostListView(ListView):
+    """
+    Alternative post list view
+    """
+    queryset = Post.published.all()
+    context_object_name = 'posts'
+    paginate_by = 5
+    template_name = 'blog/post/list.html'
 
 
 def post_list(request):
