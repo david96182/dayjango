b3fdadb7e51982b0ef8a0406182501a9543ca89b
David Puerta Martín <puertadavid96182@gmail.com>
2023-05-01 17:51:13 -0400

    add tagging to posts

--- mysite/blog/models.py
+++ mysite/blog/models.py
@@ -2,6 +2,7 @@ from django.db import models
 from django.utils import timezone
 from django.contrib.auth.models import User
 from django.urls import reverse
+from taggit.managers import TaggableManager
 
 
 class PublishedManager(models.Manager):
@@ -27,6 +28,7 @@ class Post(models.Model):
     status = models.CharField(max_length=2,
                               choices=Status.choices,
                               default=Status.DRAFT)
+    tags = TaggableManager()
 
     objects = models.Manager()  # The default manager.
     published = PublishedManager()  # Custom manager.
